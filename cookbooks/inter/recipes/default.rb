7ac62010ae82c711eceadbc784eab3f23bf6430c
gregderenne <greg.derenne@rightscale.com>
2012-09-28 16:50:54 +0000

    Added hello message to the default.rb recipe

---
+++ cookbooks/inter/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: inter
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+log "Hello folks!  RightScale with Chef rocks!"
